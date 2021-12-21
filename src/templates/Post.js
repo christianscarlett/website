b10bd9a55fda3cf40e73f3613745df1d4c542abc
Christian Scarlett <christianscarlett21@gmail.com>
2021-12-20 22:02:35 -0500

    fix left side

--- src/templates/Post.js
+++ src/templates/Post.js
@@ -1,30 +1,24 @@
 import React, { Component } from "react";
 
-import Footer from "../components/Footer";
+import Subpage from './Subpage';
+// import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-// import "../subpage.css";
 
 class Post extends Component {
   render() {
     return (
       <>
-      <Navbar projects />
+      <Subpage 
+      navbar={<Navbar projects />}
+      title={this.props.title}
+      content={(
       <div class="container-fluid" id="page-center">
-        <div class="container-fluid">
-          <div class="post-header">
-            <h1>
-              {this.props.title}
-            </h1>
-          </div>
-        </div>
-        <hr class="separator" />
-        <div class="container">
-          <div class="post-text">
-            {this.props.content}
-          </div>
+        <div class="post-text">
+          {this.props.content}
         </div>
       </div>
-      <Footer />
+      )}
+      />
       </>
     );
   }
