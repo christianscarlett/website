293e794901fd91e72b3ace8750193c10ed9b687a
Christian Scarlett <christianscarlett21@gmail.com>
2021-12-21 02:49:29 -0500

    remove cringe

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,9 +15,6 @@ class About extends Component {
             I am Christian Scarlett: engineer, musician, overall nerd, and life-long learner. Currently an undergraduate at MIT studying Electrical Engineering and Computer Science, I look for projects in my spare time to accompany my studies, like this website.
             <br/>
             <br/>
-            In the past I've worked on projects related to microcontrollers and robotics, including taking an organizational role in teaching classes I designed on those topics to 8-to-16-year-olds. Recently I've been interested in innovative, scalable software focused on impact. 
-            <br/>
-            <br/>
             If you want to know more about my work and school experiences, my resume is linked below.
             <br/>
             <br/>
