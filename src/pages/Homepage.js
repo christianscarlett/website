f6201f59af99a4bdb2e592be171825fae6c6517b
Christian Scarlett <christianscarlett21@gmail.com>
2021-12-21 15:37:56 -0500

    fix homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,16 +11,18 @@ function Homepage() {
       <Helmet>
         <title>Christian Scarlett - Home</title>
       </Helmet>
-      <Navbar index />
-      <div class="container-fluid content" id="homepage">
-        <div class="row">
-          <div class="container-fluid" id="maintext-container">
-            <h1 id="hello">
-                Sup!
-            </h1>
-            <p>
-                This is my website to showcase my projects and contact to those interested, gaining web design skills along the way.<br/>
-            </p>
+      <div id='homepage'>
+        <Navbar index />
+        <div class="container-fluid content">
+          <div class="row">
+            <div class="container-fluid" id="maintext-container">
+              <h1 id="hello">
+                  Sup!
+              </h1>
+              <p>
+                  This is my website to showcase my projects and contact to those interested, gaining web design skills along the way.<br/>
+              </p>
+            </div>
           </div>
         </div>
       </div>
