f5134270d3f29413f9144f7e36b52003eed669ce
Christian Scarlett <christianscarlett21@gmail.com>
2021-12-23 18:05:40 -0500

    added contact method styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,26 @@ import { Helmet } from 'react-helmet';
 import Navbar from '../components/Navbar';
 import Subpage from '../templates/Subpage';
 
+import MITLogo from '../images/logos/mitlogo.png';
+
+class ContactMethod extends Component {
+  render() {
+    return (
+      <>
+      <a class='contact-method-wrapper' href={this.props.link} target="_blank" rel="noopener noreferrer">
+        <div class='contact-method'>
+          <a class='contact-icon' href={this.props.link} target="_blank" rel="noopener noreferrer">{this.props.icon}</a>
+          <div class='contact-description'>
+            <h5>{this.props.title}</h5>
+            <p>{this.props.text}</p>
+          </div>
+        </div>
+      </a>
+      </>
+    );
+  }
+}
+
 class Contact extends Component {
   render() {
     return (
@@ -20,12 +40,14 @@ class Contact extends Component {
           The best way to get in touch with me is through email or social media. Feel free to email me at christianscarlett21@gmail.com and my school email cjscar@mit.edu&mdash;or, follow the icons for my other sites.
           </p>
           <div className="container-fluid">
-              <div className="contact-icons">
-                  <a href="https://www.facebook.com/chriscar2000" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"></i></a>
-                  <a href="https://twitter.com/ChriScar2000" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"></i></a>
-                  <a href="https://www.instagram.com/christian_scarlett/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"></i></a>
-                  <a href="https://www.linkedin.com/in/christianscarlett/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
-              </div>
+            <div className="contact-icons">
+              <ContactMethod link='mailto:christianscarlett21@gmail.com' icon={(<i class="fa fa-envelope"></i>)} title='Email' text='christianscarlett21@gmail.com' />
+              <ContactMethod link='mailto:cjscar@mit.edu' icon={(<img class='mit-icon' src={MITLogo} alt='MIT Logo' />)} title='MIT Email' text='cjscar@mit.edu' />
+              <ContactMethod link='https://www.facebook.com/chriscar2000' icon={(<i className="fa fa-facebook"></i>)} title='Facebook' />
+              <ContactMethod link='https://twitter.com/ChriScar2000' icon={(<i className="fa fa-twitter"></i>)} title='Twitter' />
+              <ContactMethod link='https://www.instagram.com/christian_scarlett/' icon={(<i className="fa fa-instagram"></i>)} title='Instagram' />
+              <ContactMethod link='https://www.linkedin.com/in/christianscarlett/' icon={(<i className="fa fa-linkedin"></i>)} title='LinkedIn' />
+            </div>
           </div>
           </>
         )}
