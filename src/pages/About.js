e144aa85af1d5a0ac7d57d4838547c4bbe47ba6d
Christian Scarlett <christianscarlett21@gmail.com>
2021-12-23 18:16:14 -0500

    pad content

--- src/pages/About.js
+++ src/pages/About.js
@@ -17,17 +17,19 @@ class About extends Component {
         title="About Me" 
         content = {(
           <>
-          <p>
-            I am Christian Scarlett: engineer, musician, overall nerd, and life-long learner. Currently an undergraduate at MIT studying Electrical Engineering and Computer Science, I look for projects in my spare time to accompany my studies, like this website.
-            <br/>
-            <br/>
-            If you want to know more about my work and school experiences, my resume is linked below.
-            <br/>
-            <br/>
-            <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
-          </p>
-          <div className='resume-embed'>
-            <embed src={Resume} width="100%" height="800px" />
+          <div className='pad-content'>
+            <p>
+              I am Christian Scarlett: engineer, musician, overall nerd, and life-long learner. Currently an undergraduate at MIT studying Electrical Engineering and Computer Science, I look for projects in my spare time to accompany my studies, like this website.
+              <br/>
+              <br/>
+              If you want to know more about my work and school experiences, my resume is linked below.
+              <br/>
+              <br/>
+              <a href={Resume} target="_blank" rel="noopener noreferrer">Resume</a>
+            </p>
+            <div className='resume-embed'>
+              <embed src={Resume} width="100%" height="800px" />
+            </div>
           </div>
           </>
         )}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -36,17 +36,19 @@ class Contact extends Component {
         title="Contact Me" 
         content={(
           <>
-          <p>
-          The best way to get in touch with me is through email or social media. Feel free to email me at christianscarlett21@gmail.com and my school email cjscar@mit.edu&mdash;or, follow the icons for my other sites.
-          </p>
-          <div className="container-fluid">
-            <div className="contact-icons">
-              <ContactMethod link='mailto:christianscarlett21@gmail.com' icon={(<i class="fa fa-envelope"></i>)} title='Email' text='christianscarlett21@gmail.com' />
-              <ContactMethod link='mailto:cjscar@mit.edu' icon={(<img class='mit-icon' src={MITLogo} alt='MIT Logo' />)} title='MIT Email' text='cjscar@mit.edu' />
-              <ContactMethod link='https://www.facebook.com/chriscar2000' icon={(<i className="fa fa-facebook"></i>)} title='Facebook' />
-              <ContactMethod link='https://twitter.com/ChriScar2000' icon={(<i className="fa fa-twitter"></i>)} title='Twitter' />
-              <ContactMethod link='https://www.instagram.com/christian_scarlett/' icon={(<i className="fa fa-instagram"></i>)} title='Instagram' />
-              <ContactMethod link='https://www.linkedin.com/in/christianscarlett/' icon={(<i className="fa fa-linkedin"></i>)} title='LinkedIn' />
+          <div className='pad-content'>
+            <p>
+            The best way to get in touch with me is through email or social media. Feel free to email me at christianscarlett21@gmail.com and my school email cjscar@mit.edu&mdash;or, follow the icons for my other sites.
+            </p>
+            <div className="container-fluid">
+              <div className="contact-icons">
+                <ContactMethod link='mailto:christianscarlett21@gmail.com' icon={(<i class="fa fa-envelope"></i>)} title='Email' text='christianscarlett21@gmail.com' />
+                <ContactMethod link='mailto:cjscar@mit.edu' icon={(<img class='mit-icon' src={MITLogo} alt='MIT Logo' />)} title='MIT Email' text='cjscar@mit.edu' />
+                <ContactMethod link='https://www.facebook.com/chriscar2000' icon={(<i className="fa fa-facebook"></i>)} title='Facebook' />
+                <ContactMethod link='https://twitter.com/ChriScar2000' icon={(<i className="fa fa-twitter"></i>)} title='Twitter' />
+                <ContactMethod link='https://www.instagram.com/christian_scarlett/' icon={(<i className="fa fa-instagram"></i>)} title='Instagram' />
+                <ContactMethod link='https://www.linkedin.com/in/christianscarlett/' icon={(<i className="fa fa-linkedin"></i>)} title='LinkedIn' />
+              </div>
             </div>
           </div>
           </>
